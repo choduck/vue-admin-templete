7ab61f85227e83ac81bfc398765263727d433afb
DESKTOP-TLO59CT\chodu <choduck99@gmail.com>
2021-05-25 10:25:42 +0900

    테스트

--- src/api/prodList.js
+++ src/api/prodList.js
@@ -4,9 +4,6 @@ import axios from 'axios'
 var baseURL = 'http://localhost:9999'
 
 export function getList(params) {
-  //var params1 = JSON.stringify(params)
-  //var params1 = JSON.parse(params)  
-
 
   return axios({
     //url: '/prodList2',
